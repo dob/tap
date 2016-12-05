730413af33c0b0edad234999f0f424949743f66f
Eric Tang <eric.x.tang@gmail.com>
2016-12-05 01:34:04 -0500

    new compiler

--- app/javascripts/verify.js
+++ app/javascripts/verify.js
@@ -2,8 +2,9 @@
 
 var contractAddr;
 var contractCode;
-var sampleContract = "contract mortal {\n    address owner;\n\n    function mortal() { owner = msg.sender; }\n\n}\n\ncontract greeter is mortal {\n    string greeting;\n\n    function greeter(string _greeting) public {\n        greeting = _greeting;\n    }\n\n    function greet() constant returns (string) {\n        return greeting;\n    }\n}";
-var sampleAddr = "0x7dfb4afd15c05c9dadbe92ff1183672eed510f74";
+var sampleContract = "pragma solidity ^0.4.2;\n\ncontract mortal {\n    /* Define variable owner of the type address*/\n    address owner;\n\n    /* this function is executed at initialization and sets the owner of the contract */\n    function mortal() { owner = msg.sender; }\n\n    /* Function to recover the funds on the contract */\n    // function kill() { if (msg.sender == owner) selfdestruct(owner); }\n}\n\ncontract greeter is mortal {\n    /* define variable greeting of the type string */\n    string greeting;\n\n    /* this runs when the contract is executed */\n    function greeter(string _greeting) public {\n        greeting = _greeting;\n    }\n\n    /* main function */\n    function greet() constant returns (string) {\n        return greeting;\n    }\n}";
+
+var sampleAddr = "0x0fc607602fac86c69d3b442592e8348e2a927a79";
 
 
 function error(str) {
@@ -17,6 +18,10 @@ function getContractAddr() {
 
 function getContractCode() {
     return $("#contractCode").val();
+}
+
+function getSolcVersion() {
+    return $("#versions").val();
 }
 
 function getByteCodeCompileAndVerify(addr) {
@@ -37,35 +42,31 @@ function getByteCodeCompileAndVerify(addr) {
 var compileAndVerifyContract = function(byteCode) {
     contractCode = getContractCode();
 
-    $.post("http://strato-dev2.blockapps.net/eth/v1.0/solc", { src: contractCode},
-        function( data ) {
-          // console.log( data );
-          $("#compileError").hide();
-
-          var jsonData = JSON.parse(data);
-          if (jsonData["error"] != null) {
-            error(jsonData["error"]);
-          } else {
-            var content = "";
-            var counter;
-            for(counter = 0; counter < jsonData["abis"].length; counter++) {
-                abi = jsonData["abis"][counter];
-                contract = jsonData["contracts"][counter];
-                content += "<div id='contract_" + abi["name"] + "' style='margin:10px'>" + 
-                    "<label style='font-style:italic'>" + abi["name"] + "</label>" + "<br/>" + 
-                    "<label>Interface: </label>" + "<input readonly='readonly' value='" + JSON.stringify(abi["abi"]) + "'></input>" + 
-                    "<label>Bytecode: </label>" + "<input readonly='readonly' value='" + contract["bin"] + "'></input>" +
-                    "</div>";
-            }
-            content = "<div>Solc Compile Result</div>" + content;
-            $("#compileResult").html(content);
-
-            compiledContracts = jsonData["contracts"];
-            // console.log("Calling verify: " + byteCode + JSON.stringify(compiledContracts));
-            verifyCode(compiledContracts, byteCode);
-          }
+    BrowserSolc.loadVersion(getSolcVersion(), function(compiler) {
+        optimize = 1;
+        result = compiler.compile(getContractCode(), optimize);
+        console.log(result);
+        var content = "";
+        var counter;
+        contracts = Object.keys(result["contracts"]);
+        for(counter = 0; counter < contracts.length; counter++) {
+            name = contracts[counter];
+            console.log(name);
+            compilerOutput = result["contracts"][name];
+            // contract = result["contracts"][counter];
+            content += "<div id='contract_" + name + "' style='margin:10px'>" + 
+                "<label style='font-style:italic'>" + name + "</label>" + "<br/>" + 
+                "<label>Interface: </label>" + "<input readonly='readonly' value='" + compilerOutput["interface"] + "'></input>" + 
+                "<label>Bytecode: </label>" + "<input readonly='readonly' value='" + compilerOutput["bytecode"] + "'></input>" +
+                "</div>";
         }
-    );
+        content = "<div>Solc Compile Result</div>" + content;
+        $("#compileResult").html(content);
+
+        // compiledContracts = jsonData["contracts"];
+        // verifyCode(compiledContracts, byteCode);
+
+    });
 }
 
 var verifyCode = function(compiledContracts, byteCode) {
@@ -87,6 +88,18 @@ var verifyCode = function(compiledContracts, byteCode) {
         error("Contract bytecode mismatch. Cannot verify contract at Address: " + getContractAddr());
     } else {
         alert("Found a match!  Verificatoin success!");
+    }
+}
+
+function populateVersions(versions) {
+    sel = document.getElementById("versions");
+    sel.innerHTML = "";
+
+    for(var i = 0; i < versions.length; i++) {
+        var opt = document.createElement('option');
+        opt.appendChild( document.createTextNode(versions[i]) );
+        opt.value = versions[i]; 
+        sel.appendChild(opt); 
     }
 }
 
@@ -131,5 +144,9 @@ window.onload = function() {
 
     });
 
+
+    BrowserSolc.getVersions(function(soljsonSources, soljsonReleases){
+        populateVersions(soljsonSources);
+    });
     // getNetwork();
 }
--- truffle.js
+++ truffle.js
@@ -3,11 +3,14 @@ module.exports = {
     "index.html": "index.html",
       "attestation.html": "attestation.html",
       "addAttestation.html": "addAttestation.html",
+      "verify.html": "verify.html",
     "app.js": [
       "javascripts/app.js"
     ],
       "add.js":"javascripts/add.js",
       "ipfs.js":"javascripts/ipfs.js",
+      "verify.js":"javascripts/verify.js",
+      "browser-solc.min.js":"javascripts/browser-solc.min.js",
       "app.css": [
       "stylesheets/app.css"
     ],
